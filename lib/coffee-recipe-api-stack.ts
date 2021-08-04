4ca715240174fb8b51d6561d663c84a5eeb005c1
Josua Blejeru <37913833+josuablejeru@users.noreply.github.com>
2021-08-04 18:45:14 +0300

    Initial commit

---
+++ lib/coffee-recipe-api-stack.ts
@@ -0,0 +1,9 @@
+import * as cdk from '@aws-cdk/core';
+
+export class CoffeeRecipeApiStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+  }
+}
